790f5945c4e4ec43d0db22a749fa65ba95c0ffe7
Thowfeeq Ahmed Syed <thowfeeqahmedsyed@Thowfeeqs-MacBook-Air.local>
2023-01-26 14:23:22 +0530

    fixed , now its working

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import {Card} from '../card/card.component.jsx'
-import './card-list.styles.css'
+import React from "react";
+import { Card } from "../card/card.component.jsx";
+// import './card-list.styles.css'
 
-export const CardList =  (props) => {
-  
+export const CardList = (props) => {
   return (
-    <div className='card-list'>
-
-    {
-        props.monster.map((monster) => (
-          <Card key={monster.id} monster={monster} newGroup={props.changeto}/>
+    <div className="card-list">
+      {props.monster.map((monster) => (
+        <Card key={monster.id} monster={monster} newGroup={props.changeto} />
         // <h1 key={monster.id}> { monster.name }</h1>
-      ))
-    }
+      ))}
     </div>
-  )
-}
+  );
+};
